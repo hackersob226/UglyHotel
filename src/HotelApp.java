cfb40591f6757fe653af3efb16f0d1c82d8ed9e7
Ambrose Cheung <cheungambrose30@gmail.com>
2015-11-27 02:53:15 -0500

    Starting on JDBC stuff
    Starting on JDBC stuff in main

--- src/HotelApp.java
+++ src/HotelApp.java
@@ -1,16 +1,22 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.sql.*;
+import javax.sql.*;
 
 public class HotelApp {
     public static final int WIDTH = 200, HEIGHT = 400;
+    public static Connection conn;
 
     static JPanel layout;
     static String currentState = "Login";
     
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Hotel Application");
+        JFrame frame = new JFrame("Fancy Hotels");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        conn = getConnection();
         
         layout = new JPanel(new CardLayout());
         layout.setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -30,5 +36,21 @@ public class HotelApp {
     public static void checkState() {
         CardLayout c = (CardLayout)(layout.getLayout());
         c.show(layout, currentState);
+    }
+    
+    public static Connection getConnection() throws SQLException {
+        Connection aConn = null;
+        //Properties connectionProps = new Properties();
+        //connectionProps.put("user", "cs4400_Group_65");
+        //connectionProps.put("password", "Nj7gOvKI");
+
+        //conn = DriverManager.getConnection(
+        //            "jdbc:mysql://" +
+        //            "PLACEHOLDER" +
+        //            ":" + "PLACEHOLDER" + "/",
+        //            connectionProps);
+        
+        System.out.println("Connected to database");
+        return conn;
     }
 }
