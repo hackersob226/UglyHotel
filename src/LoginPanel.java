733c3d5c82a74609093224a290fb483fcd9e432b
Ambrose Cheung <cheungambrose30@gmail.com>
2015-11-27 21:20:28 -0500

    SQL Progress on LoginPanel

--- src/LoginPanel.java
+++ src/LoginPanel.java
@@ -3,10 +3,13 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
+import java.sql.*;
+import javax.sql.*;
 
 public class LoginPanel extends JPanel {
     private JButton login, newUser;
     private JTextField userName, pass;
+    private String enteredUserName, enteredPassword;
 
     public LoginPanel() {
         userName = new JTextField(15);
@@ -36,8 +39,52 @@ public class LoginPanel extends JPanel {
         }
 
         public void actionPerformed(ActionEvent e) {
+            int userType = 0;
+            enteredUserName = userName.getText();
+            enteredPassword = pass.getText();
+
+            // try {
+            //     userType = checkUser(HotelApp.conn, "hoteldb");
+            // } catch (SQLException ex ) {
+            //     System.out.println("Error");
+            // }
+            
+            if(userType == 1)
+            {
+                HotelApp.currentState = "CustomerView";
+                HotelApp.checkState();
+            }else if(userType == 2)
+            {
+                HotelApp.currentState = "ManagerView";
+                HotelApp.checkState();
+            }else
+            {
+                //TODO
+                //Invalid Login or some Error
+            }
             HotelApp.currentState = state;
             HotelApp.checkState();
         }
+    }
+
+    public int checkUser(Connection con, String dbName) throws SQLException {
+
+        Statement stmt = null;
+        String query = "select USER, PASSWORD from " + dbName + ".USER";
+        try {
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                if(enteredUserName == rs.getString("USER") && enteredPassword == rs.getString("PASSWORD"))
+                {
+                    return 1;
+                }
+            }
+        } catch (SQLException e ) {
+            System.out.println("Could not find user.");
+        } finally {
+            if (stmt != null) { stmt.close(); }
+        }
+        return 0;
     }
 }
